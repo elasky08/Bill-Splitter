814939ece1448acdc0d2fcb1f9e79f1cee8db2b9
Jonathan Allen <jallen01@mit.edu>
2013-10-23 17:42:33 -0400

    Created new Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BillSplitter::Application.config.secret_key_base = 'd36f3d536c39d131200478a43c029502cce75f34df7e827e291fdabe5eff72d9495f70e79f8339d4d9b0bd493434c9d93403724a3eff43906f7f95dbca3a9d3b'
