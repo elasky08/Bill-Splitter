86f82b1d0c9bc42efecae1353b2a80cdf5cb6e08
Paula Jacobs <paula.jacobso@gmail.com>
2013-10-27 19:45:57 -0400

    Bill modal fixed

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
 		$('.alert').fadeOut();
 	});
 
-	$('#get-bill').click(function() {
+	$('.get-bill').click(function() {
 		getBillSummary();
 	});
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131025012041) do
+ActiveRecord::Schema.define(version: 20131025174659) do
 
   create_table "group_users", force: true do |t|
     t.integer  "group_id"
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(version: 20131025012041) do
     t.integer  "owner_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
+
+  add_index "groups", ["slug"], name: "index_groups_on_slug"
 
   create_table "items", force: true do |t|
     t.decimal  "cost",       precision: 8, scale: 2, null: false
@@ -57,9 +60,11 @@ ActiveRecord::Schema.define(version: 20131025012041) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "slug"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["slug"], name: "index_users_on_slug", unique: true
 
 end
