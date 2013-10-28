fe4479f9def4dd6baeba6d32124fef09ff3a3f3b
Charles Liu <cliu2014@mit.edu>
2013-10-27 23:04:34 -0400

    Commented JS, primary authors

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,20 +1,26 @@
+// PRIMARY AUTHOR: Charles Liu (cliu2014)
 $(document).ready(function() {
 	
 	var $addItemArea = $('#add-item-area');
 	var MAX_LENGTH = 20;
 	var request;
 
+	// Bind click handler to the add item button. Make it show the input fields to add an item
 	$('#show-add-item').click(function() {
 		disableInput(false);
 		$addItemArea.slideDown('fast').find('#item-name').focus();
 	});
 
+	// When an item is added, first check that the length is valid (between 1-20), and that the cost is a number
+	// If valid inputs, perform the AJAX request to add the item, and add the line to the table
 	$('#add-item').click(function() {
 		var itemName = $('#item-name').val();
 		var itemCost = extractCost($('#item-cost').val());
+
+		// Validate length of itemname
 		if (itemName.length === 0 || itemName.length > MAX_LENGTH) {
 			$('#add-item-error').text('Name must be 1-20 characters in length').fadeIn();
-		} else if (isNaN(itemCost)) {
+		} else if (isNaN(itemCost)) { // Validate cost is a number
 			$('#add-item-error').text('Cost must be a valid number').fadeIn();
 		} else {
 			itemCost = parseFloat(itemCost);
@@ -32,6 +38,7 @@ $(document).ready(function() {
 		}
 	});
 
+	// When the cancel button is clicked, abort any requests that have not come back, and hide the add item area
 	$('#cancel-add-item').click(function() {
 		if (request) {
 			request.abort();
@@ -40,6 +47,7 @@ $(document).ready(function() {
 		$('#add-item-error').hide();
 	});
 
+	// Hide errors when the user clicks/focuses into an input field
 	$addItemArea.find('input').focus(function() {
 		$('#add-item-error').fadeOut();
 	});
@@ -51,11 +59,14 @@ $(document).ready(function() {
 	$('#recompute-bill').click(function() {
 		computeBillSummary();
 	});
-
+	
+	// Hide errors when the user clicks/focuses into an input field
 	$('#bill-modal input[type="text"]').focus(function() {
 		$('#invalid-bill-input-error').fadeOut();
 	});
 
+	// When the delete link is clicked, do an AJAX request to delete the item from the group
+	// Callback returns false so that the link is not followed
 	$('#group-items').on('click', '.delete-item', function() {
 		var url = $(this).attr('href');
 		var $line = $(this).parents('tr');
@@ -67,6 +78,9 @@ $(document).ready(function() {
 		return false;
 	});
 
+	// Gets the current url path without the domain or any additional params:
+	// For example, returns /groups/1
+	// The URL path returned by this function will not have the trailing '/'
 	var getURL = function() {
 		var url = window.location.pathname;
 		if (url.slice(-1) === '/') {
@@ -80,6 +94,8 @@ $(document).ready(function() {
 		$('#add-item').attr('disabled', isDisabled);
 	};
 
+	// Get the cost from the input field
+	// Handles the case where there are spaces on both sides, as well as if the user inputs a dollar sign
 	var extractCost = function(costString) {
 		var modifiedCostString = costString.trim();
 		if (modifiedCostString.charAt(0) === "$") {
@@ -88,6 +104,7 @@ $(document).ready(function() {
 		return modifiedCostString;
 	};
 
+	// Create a new row in the group's table; called when an item is added
 	var newRow = function(id, name, cost) {
 		var costStr = '$' + parseFloat(cost).toFixed(2);
 		var $deleteLink = $('<td />').append($('<a />').attr('href', getURL() + '/items/' + id).text('Delete').addClass('delete-item'));
@@ -95,6 +112,10 @@ $(document).ready(function() {
 		$('#group-items').append($tr);
 	};
 
+	// Get the bill summary
+	// Figures out the sum of item costs via an AJAX call,then populates the tax and tip field 
+	// to calculate an even split
+	// Note that we assume 7.25% tax, 15% tip, and 4 people splitting by default
 	var getBillSummary = function() {
 		$.get(getURL() + '/cost/', function(data) {
 			populateBillSummary(data);
@@ -102,16 +123,21 @@ $(document).ready(function() {
 		}, 'json');
 	};
 
+	// The callback for the bill summary AJAX request, followed by calculations using default tax/tip
 	var populateBillSummary = function(cost) {
 		$('#item-total').text(cost.toFixed(2));
 		computeBillSummary();
 	};
 
+	// Computes the total bill and split costs based on the input fields
+	// Validates the inputs, then does the necessary computations
+	// Note that the tip is calculated relative to the subtotal.
 	var computeBillSummary = function() {
 		var subtotal = parseFloat($('#item-total').text());
 		var tax = $('#tax-pct').val();
 		var tip = $('#tip-pct').val();
 		var numPeople = $('#num-people').val();
+		// Validate all inputs are proper numbers
 		if (isNaN(tax) || isNaN(tip) || isNaN(numPeople)) {
 			$('#invalid-bill-input-error').fadeIn();
 		}
