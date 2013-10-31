4180551a8eba0ec7d18c0803b602a13d761916a0
Jonathan Allen <jallen01@mit.edu>
2013-10-31 19:13:13 -0400

    Adding more partials

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -4,28 +4,28 @@
 class GroupUsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_group
-  before_action :set_user, except: [:create]
+  before_action :set_user, except: [:index, :create]
+
+  def index
+  end
 
   # Add user to group
   def create
     @user = User.find_by(email: group_user_params[:email])
 
-    # If user id or group id is invalid, render 404.
-    unless @user
-      respond_to do |format|
-        format.js { "groups/" }
+    respond_to do |format|
+      # If user id or group id is invalid, render 404.
+      unless @user
+        format.js { render "groups/" }
         format.json { render status: :not_found }
-      end
-    end 
+      end 
 
-    # If current user does not have permission to add user to group, render 403.
-    unless @group.owner == current_user
-      respond_to do |format|
+      # If current user does not have permission to add user to group, render 403.
+      unless @group.owner == current_user
+        format.html {  }
         format.json { render status: :forbidden }
       end
-    end
 
-    respond_to do |format|
       @group_user = @group.add_user(user)
       if @group_user
         format.json { render json: @group_user, status: :created }
@@ -62,20 +62,21 @@ class GroupUsersController < ApplicationController
 
   # Remove user from group
   def destroy
-    # If current user does not have permission to remove user from group, render 403.
-    unless [@group.owner, @user].include?(current_user)
-      format.json { render status: :forbidden }
-    end
-
     respond_to do |format|
+      # If current user does not have permission to remove user from group, render 403.
+      unless [@group.owner, @user].include?(current_user)
+        format.json { render status: :forbidden }
+      end
+
       @group.remove_user(user)
+        
       format.json { render status: :ok }
     end
   end
 
   private
     def set_group
-      @group = Group.find(params[:group_id])
+      @group = Group.includes(:group_users, :users).find(params[:group_id])
 
       # If group does not exist, render 404.
       unless @group
