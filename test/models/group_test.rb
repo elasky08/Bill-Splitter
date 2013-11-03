04450b2d7855eaa629ad727affae10518e91cf96
Paula Jacobs <paula.jacobso@gmail.com>
2013-11-03 18:56:40 -0500

    Added tests, payment test not yet passed

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,8 +1,72 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
+  group = FactoryGirl.create(:group, name: "Paula's Group", id:26)
+  user = FactoryGirl.create(:user, name: "Paula", id:25)
+
   test "create group" do
-    group = FactoryGirl.build(:group)
+    test_group = FactoryGirl.build(:group, name: "Paula's Test Group")
+    assert_equal Group.new.class, test_group.class, "This is not a group"
+	assert_equal "Paula's Test Group", test_group.name, "The group name is not being set"
+  end
+
+  test "add user" do
+    assert (not group.includes_user?(user)), "The user should not be added yet"
+    assert group.add_user(user), "Add User Should return True"
+    assert (group.includes_user?(user)), "The user should be added"
+  end
+
+  test "remove user" do
+    assert (not group.includes_user?(user)), "The user should not be added yet"
+    assert group.add_user(user), "Add User Should return True"
+    assert (group.includes_user?(user)), "The user should be added" 
+    group.remove_user(user)
+    assert (not group.includes_user?(user)), "The user should not be in group"
+  end
+
+  test "get membership" do
+    assert (not group.includes_user?(user)), "The user should not be added yet"
+    assert group.add_user(user), "Add User Should return True"
+    assert (group.includes_user?(user)), "The user should be added"
+    membership = group.get_membership(user) 
+	assert_equal group.id, membership.group_id, "Group is not in membership with user"
+	assert_equal user.id, membership.user_id, "Group is not in membership with user"
     
   end
+
+  test "add item" do
+    assert group.add_user(user), "Add User Should return True"
+    item = group.edit_item_by_name("Steak", 5.00)
+    item_two = group.edit_item_by_name("Bacon", 7.00)
+    assert_equal 12.00, group.get_items_total, "Total should be equal to 5"
+    
+  end
+
+  test "get user item and totals" do
+    assert group.add_user(user), "Add User Should return True"
+    item = group.edit_item_by_name("Ham", 6.00)  
+    item.add_user(user)
+    assert_equal item.name, group.get_user_items(user).first.name, "The item should be added to user"
+    assert_equal item.cost, group.get_user_total(user), "The cost should be equal"    
+
+  end
+
+  test "get payment" do
+  	user_two = FactoryGirl.create(:user, name: "Steve", id:26)
+    assert group.add_user(user), "Add User Should return True"
+    assert group.add_user(user_two), "Add User Should return True"
+
+    membership = group.get_membership(user)
+    membership.payment = 12.00
+    
+    assert_equal membership.payment, group.get_payments_total, "The payments should be equal"
+    
+
+  end
+
+
+
+
+
+
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  item = FactoryGirl.create(:item, name: "Steak", cost:26)
+  user = FactoryGirl.create(:user, name: "Paula", id:25)
+  user_two = FactoryGirl.create(:user, name: "Joe", id:23)
+
+  test "create group" do
+    assert_equal Item.new.class, item.class, "This is not an item"
+	assert_equal "Steak", item.name, "The item name is not being set"
+	assert_equal BigDecimal.new("26"), item.cost, "The item cost is not being set"
+  end
+
+  test "add user" do
+    assert (not item.includes_user?(user)), "The user should not be added yet"
+    assert_equal 0, item.count_users, "There should be zero users"
+
+    assert item.add_user(user), "Add User Should return True"
+    assert_equal 1, item.count_users, "There should be one user"
+    assert (item.includes_user?(user)), "The user should be added"
+  end
+
+  test "remove user" do
+    assert (not item.includes_user?(user)), "The user should not be added yet"
+    assert item.add_user(user), "Add User Should return True"
+    assert (item.includes_user?(user)), "The user should be added" 
+    assert_equal 1, item.count_users, "There should be one user"
+    item.remove_user(user)
+    assert (not item.includes_user?(user)), "The user should not be in group"
+  	assert_equal 0, item.count_users, "There should be zero users"
+  end
+
+  test "get partition" do
+    assert item.add_user(user), "Add User Should return True"
+    partition = item.get_partition(user) 
+	assert_equal item.id, partition.item_id, "User does not eat this item"
+	assert_equal user.id, partition.user_id, "User does not eat this item"
+    
+  end
+
+  test "get cost" do
+    assert item.add_user(user), "Add User Should return True"
+    assert item.add_user(user_two)
+	assert_equal 13, item.user_cost, "Cost does not meet expected"
+    
+  end
+
+
+
+  
 end
