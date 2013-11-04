ca8ca4c4727fc6374792f134a300a6ae3211e832
Charles Liu <cliu2014@mit.edu>
2013-11-03 23:37:18 -0500

    Add me link

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -3,11 +3,18 @@
 // Reload items list automatically every few seconds.
 $(document).ready(function() {
 	if ($('#items-list').length) {
+		var path = window.location.pathname;
+
+		// If the address has a trailing slash, don't include it in the ajax call
+		// This way, we don't get things like /groups/1/.js (which is invalid)
+		if (path.slice(-1) === '/') {
+			path = path.substring(0, path.length - 1);
+		}
 		setInterval(function() {
-			$.get(window.location.pathname + '.js');
+			$.get(path + '.js');
 		}, 5000);
 	}
-});
+});	
 
 // Hide item's user list when group_expense option is checked.
 $(document).on('shown', '#edit-item-modal', function() {
@@ -19,4 +26,11 @@ $(document).on('shown', '#edit-item-modal', function() {
 // Show item's user list when group_expense option is unchecked.
 $(document).on('change', '#item_group_expense', function() {
 	$('#item_group_expense').is(':checked') ? $('.edit-users').slideUp() : $('.edit-users').slideDown();
+});
+
+$(document).on('click', '#add-me', function() {
+	var $link = $(this);
+	var url = $link.attr('href');
+	$.post(url, { 'email': $link.data('email') });
+	return false;
 });
